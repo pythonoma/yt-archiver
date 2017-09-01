dbd3dec4e9b09e456e9c45188edff6704dbe855b
pythonoma <islamona2008@gmail.com>
2017-09-02 01:36:36 +0300

    Update yt-archiver-install.sh

--- yt-archiver-install.sh
+++ yt-archiver-install.sh
@@ -9,6 +9,7 @@ echo "Installing aria2 ...";
 echo "-----------------------------";
 eval "apt-get -y install aria2";
 # fix error 22 https://github.com/aria2/aria2/issues/502
+eval "mkdir -p ~/.aria2";
 eval "echo 'async-dns=false' >> ~/.aria2/aria2.conf";
 
 echo "=================================";
