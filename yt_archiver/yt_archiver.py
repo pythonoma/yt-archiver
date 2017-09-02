05971286797993f3333977311301afd41ec045d5
pythonoma <islamona2008@gmail.com>
2017-09-02 11:43:50 +0300

    Multithreading for upload/download

--- yt_archiver/yt_archiver.py
+++ yt_archiver/yt_archiver.py
@@ -10,11 +10,22 @@ import youtube_dl
 import os
 import sys
 
+import threading
+
 # from archive_uploader import upload_to_archive
 
 from sys import stdout
 from time import sleep
 
+is_finished_downloading = False
+is_uploading = False
+
+downloaded_count = 0
+uploaded_count = 0
+failed_download_list = []
+failed_upload_list = []
+
+cwd = os.getcwd()
 
 def create_archive_identifier( identifier):
     """
@@ -105,8 +116,15 @@ class MyLogger(object):
 
 def my_hook(d):
     if d['status'] == 'finished':
-        print('+Downloaded ' + d['filename'])
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('*************************************************************')
+        print('=>>> Downloaded ' + d['filename'])
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        global downloaded_count 
+        downloaded_count +=1
+
+    if d['status'] == 'error':
+        global failed_download_list
+        failed_download_list.append(d['filename'])
 
     # if d['status'] != 'downloading':
     #     stdout.write("\n")
@@ -117,7 +135,97 @@ def my_hook(d):
         stdout.flush()
         sleep(1) # move the cursor to the next line
 
-def yt_archiver(url, identifier, hide_date=False, hide_id=False, hide_format=False):
+
+def print_status_string(ia_id, downloads_path):
+    global uploaded_count, failed_upload_list, downloaded_count, failed_download_list
+    successful_downloads = downloaded_count - len(failed_download_list)
+    successful_uploads = uploaded_count - len(failed_upload_list)
+    failed_downloads_file = os.path.join('downloads', ia_id + '.failed_downloads')
+    failed_uploads_file = os.path.join('downloads', ia_id + '.failed_uploads')
+    
+    with open( failed_downloads_file, 'w') as f:
+        for v in failed_download_list:
+            f.write(v + '\n')
+    
+    with open( failed_uploads_file, 'w') as f:
+        for v in failed_upload_list:
+            f.write(v + '\n')
+    
+
+
+    status_string = '\n\n\n///////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\' + \
+                    '\n                                           Backup Summary                                               ' + \
+                    '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + \
+                    '\n>>>>>>>>>>> Successfuly downloaded: ' + str(successful_downloads) + '/' + str(downloaded_count) + ',  ' + \
+                    'Failed: ' + str(len(failed_download_list)) + '. <<<<<<<<<<<\n' + \
+                    '\n>>>>>>>>>>> Successfuly uploaded: ' + str(successful_uploads) + '/' + str(uploaded_count) + ',  '+ \
+                    'Failed: ' + str(len(failed_upload_list)) + '. <<<<<<<<<<<\n\n' + \
+                    'Failed download list saved to "' + str(failed_downloads_file) + ' .\n' + \
+                    'Failed upload list saved to "' + str(failed_uploads_file) + ' .\n' + \
+                    '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////////////////////////\n\n\n'
+                    
+    stdout.write(status_string)
+    
+def is_downloads_path_empty(downloads_path):
+    if os.path.exists(downloads_path):
+        files = os.listdir(downloads_path)
+        for file in files:
+            if file.endswith(".mp4") or file.endswith(".webm") or \
+                file.endswith(".3gp") or file.endswith(".flv") or \
+                file.endswith(".mp3") or file.endswith(".m4a"):
+                return False
+        return True
+    else:
+        return True
+
+def upload_downloaded_thread(ia_id, downloads_path):
+    global is_finished_downloading, is_uploading
+
+    while not is_finished_downloading or not is_downloads_path_empty(downloads_path):
+        print_status_string(ia_id, downloads_path)
+        sleep(20)
+        stdout.write('\n=================is_uploading: ' + str(is_uploading))        
+        if is_uploading:
+            # pass
+            stdout.write('\n')
+            print('=====uploading, skipping check')
+        else:
+            # stdout.write('\n')
+            print('=====Upload thread checking:' + downloads_path)
+            item = get_item(ia_id)
+            md = dict(mediatype='movies',)
+
+            if os.path.exists(downloads_path):
+                files = os.listdir(downloads_path)
+                for file in files:
+                    if file.endswith(".mp4") or file.endswith(".webm") or \
+                        file.endswith(".3gp") or file.endswith(".flv") or \
+                        file.endswith(".mp3") or file.endswith(".m4a"):
+                        try:
+                            filepath = os.path.join(downloads_path, file)
+                            is_uploading = True
+                            item.upload(filepath,  metadata=md, delete=True, checksum=True)
+                            
+                            stdout.write('\n')
+                            print('**********************************************************')
+                            print('uploaded: ' + str(file))
+                            print('==========================================================')
+                            global uploaded_count 
+                            uploaded_count += 1
+                        except Exception as ex:
+                            stdout.write('\n')
+                            print('**********************************************************')                            
+                            print(ex)
+                            print('Failed to upload:' + str(file))
+                            print('==========================================================')
+                            global failed_upload_list 
+                            failed_upload_list.append(filepath)
+                is_uploading = False            
+
+      
+
+
+def yt_archiver(url, identifier, hide_date=False, hide_id=True, hide_format=True):
     """
     1. Download youtube playlist or channel from 'url' with 'best' quality using 'aria2c'.
     2. Upload downloaded videos to Archive.org 'identifier'
@@ -128,17 +236,18 @@ def yt_archiver(url, identifier, hide_date=False, hide_id=False, hide_format=Fal
     hide_format: Hide youtube video format from file name.   
 
     """
+    downloads_path = os.path.join(os.getcwd()+ '/downloads', identifier)
     ydl_opts = {
         # 'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'format': 'best',
         # 'format': 'worst',
         # 'ignoreerrors': 'True',
         'external_downloader': 'aria2c',
-        'download_archive': os.path.join('downloads', identifier+ '.download-archive') , 
+        'download_archive': downloads_path+ '.download-archive' , 
         'logger': MyLogger(),
         'progress_hooks': [my_hook],
     }
-    output_t = os.path.join('downloads', identifier) + '/'
+    output_t = downloads_path + '/'
     if not hide_date:
         output_t += '%(upload_date)s-'
     output_t += '%(title)s'
@@ -149,10 +258,14 @@ def yt_archiver(url, identifier, hide_date=False, hide_id=False, hide_format=Fal
     
     ydl_opts['outtmpl'] = output_t + '.%(ext)s'
 
+    background_thread = threading.Thread(target=upload_downloaded_thread, args=(identifier, downloads_path,))
+    background_thread.start()
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         print('============================')
         print('Downloading Youtube videos...')
         ydl.download([url])
+
 
 def skip_yt_video(video_id, identifier):
     print("Skipping "+ video_id)
@@ -164,8 +277,8 @@ def main():
     parser.add_argument('-u','--url', help='Url of Youtube channel or playlist.', required=True)
     parser.add_argument('-i','--identifier', help='Identifier for archive.org page.', required=True)
     parser.add_argument('-hd','--hidedate', help='Hide youtube video upload date from filename .', action='store_true', default=False)
-    parser.add_argument('-hid','--hideid', help='Hide youtube video id from filename .', action='store_true', default=False)
-    parser.add_argument('-hf','--hideformat', help='Hide youtube video format from filename .', action='store_true', default=False)
+    parser.add_argument('-hid','--hideid', help='Hide youtube video id from filename .', action='store_true', default=True)
+    parser.add_argument('-hf','--hideformat', help='Hide youtube video format from filename .', action='store_true', default=True)
 
     args = vars(parser.parse_args())
     yt_url = args['url']
@@ -181,20 +294,20 @@ def main():
         while True:
             try:
                 yt_archiver(yt_url, ia_id, hide_date=arg_hide_date, hide_id=arg_hide_id, hide_format=arg_hide_format)
-                print('===Finished Downloading, Uploading to "' + ia_id +'"')
+                print('===Finished Downloading==========')
+                print_status_string(ia_id,downloads_folder)
+                global is_finished_downloading
+                is_finished_downloading = True
                 # No errors occured during download, Upload them...
-                upload_to_archive(ia_id, downloads_folder)
+                # upload_to_archive(ia_id, downloads_folder)
                 break
             except DownloadError as ex:
                 print('=======Download Error')
                 # print(ex)
                 ex = str(ex)
                 if ex.find('aria2c exited with code 9') > -1 :   
-                    print('=======No Space left on disk, uploading to free space...')               
-                    upload_to_archive(ia_id, downloads_folder)
-                    continue
-                elif ex.find('aria2c exited with code 1') > -1 :   
-                    print('=======Unknown error occcured, Sleeping for 10 seconds and retrying...')               
+                    print('=======No Space left on disk, waiting 10 seconds for uploading to free space...')               
+                    # upload_to_archive(ia_id, downloads_folder)
                     sleep(10)
                     continue
                 
@@ -214,15 +327,24 @@ def main():
 
                 elif ex.find('aria2c exited with code 16') > -1 :   
                     print('=======could not create new file or truncate existing file....')
-                    exit 
+                    print('try running yt-archiver <Yotube_URL> <Archive_Identifier> -hid -hf')
+                    exit    
                 
                 elif ex.find('aria2c exited with code 17') > -1 :   
-                    print('=======file I/O error occurred.....')
-                    upload_to_archive(ia_id, downloads_folder)
+                    print('=======file I/O error occurred, sleeping 10 seconds.....')
+                    # upload_to_archive(ia_id, downloads_folder)
+                    sleep(10)
+
 
                 elif ex.find('aria2c exited with code 18') > -1 :   
                     print('=======aria2 could not create directory.....')
                     exit 
+                
+                elif ex.find('aria2c exited with code 1') > -1 :   
+                    print('=======Unknown error occcured, Sleeping for 10 seconds...')               
+                    sleep(10)
+                    # upload_to_archive(ia_id, downloads_folder)
+                    continue
                     
                 elif ex.find('account associated with this video has been terminated') > -1 or \
                         ex.find('blocked it on copyright grounds') > -1:
